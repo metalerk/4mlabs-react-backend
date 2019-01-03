5909175926fcf44a227c582d36f69f0c14442d8e
Luis Esteban Rodriguez <rodriguezjluis0@gmail.com>
2019-01-02 19:51:03 -0600

    clean

--- api/__init__.py
+++
@@ -1,9 +0,0 @@
-from flask_restful import Resource
-from flask import jsonify
-
-import json
-
-class StoreInfo(Resource):
-    def post(self):
-        info = self.parser.parse_args()
-        print(info)
